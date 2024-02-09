22cf3899da4ca413a73fe49f5df3724909246bb0
khushboo-jamali <jamalikhushboo@gmail.com>
2024-02-09 19:18:47 +0100

    start js

---
+++ my.js
@@ -0,0 +1,42 @@
+let name1 = "Name : ";
+let name = "My name is khushboo";
+
+let code = "My bage code is 2310f";
+let bcode = "Code : ";
+let c = "Thankyou";
+
+let email = "khushboojamali6@email.com";
+let emailu = "Email : ";
+
+let contact = 3003252919;
+let contactnum = " Contact : ";
+let t = true;
+let tf = " True/False  : ";
+
+let money = "Rs :";
+let number = 190;
+let numbers = 80;
+
+console.log("*Hello Sir*");
+console.log(name1, name);
+console.log(bcode, code);
+
+console.log(emailu, email);
+console.log(contactnum, contact);
+console.log(tf, t);
+console.log(money, number + numbers);
+console.log(c);
+
+document.write("<h1><i>*Hello Sir* </i></h1>");
+document.write("" + name1, name);
+document.write("<br><br>", bcode, code);
+document.write("<br><br>", emailu, email);
+document.write("<br><br>", contactnum, contact);
+document.write("<br><br>", tf, t);
+document.write("<br><br>", money, number + numbers);
+document.write("<br><br>", c);
+
+// console.log("hello world"+name,code,email);
+// let val = "123" //number
+// let val2 = true //boolean
+// console.log(val+val2)
